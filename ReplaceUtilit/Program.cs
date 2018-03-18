d4ef8f502e0d75c582501e4889a6d6c3143b55a2
Eugen Moninets <darkstarmacho@gmail.com>
2018-03-18 21:22:44 +0200

    Finish

--- ReplaceUtilit/Program.cs
+++ ReplaceUtilit/Program.cs
@@ -12,12 +12,7 @@ namespace ReplaceUtilit
     {
         static void Main(string[] args)
         {
-            //StreamReader reading = File.OpenText(@"D:\titles.txt");
-
-            //string str = reading.ReadToEnd();
-            //Console.WriteLine(str);
-           ReadTextFile();
-            
+            ReadTextFile();
 
             Console.ReadKey();
         }
@@ -28,21 +23,36 @@ namespace ReplaceUtilit
 
             foreach (string line in lines)
             {
-                Console.WriteLine("Original: {0}", line);
-                Console.WriteLine("Replaced: {0}", ReFormat(line));
+                Console.WriteLine(ReFormat(line));
             }
         }
 
         private static string ReFormat(string str)
         {
-            string PATTERN = @"(?<=[A-Za-z]+)(\dm\s\d{2}s)";
-            string REPLACE_PATTERN = @"(?<=[A-Za-z]+)(\dm\s\d{2}s)";
+            string PATTERN = @"(?<=[A-Za-z\?]+|201[5,7]|\s)([0-9]?[0-9]m\s\d{1,2}s)";
+            string REPLACE_PATTERN = " ($1)";
+            string REPLACE_TITLE_PATTERN = " ($1)" + "\n\n";
 
-            Match match = Regex.Match(str, PATTERN);
-
-            if (match.Success)
+            if (Regex.IsMatch(str, @"\b([A-Za-z]+(?:\dm\s\d\d?s))\b"))
             {
-                str = Regex.Replace(str, PATTERN, REPLACE_PATTERN, RegexOptions.IgnoreCase);
+                str = Regex.Replace(str, PATTERN, REPLACE_PATTERN);
+            }
+            else if (Regex.IsMatch(str, @"\b([A-Za-z]+(?:\d\dm\s\d\d?s))\b"))
+            {
+                str = Regex.Replace(str, PATTERN, REPLACE_TITLE_PATTERN);
+                str = "\n" + str;
+            }
+            else if (Regex.IsMatch(str, @"\b([\?]+(?:\dm\s\d\d?s))\b"))
+            {
+                str = Regex.Replace(str, PATTERN, REPLACE_PATTERN);
+            }
+            else if (Regex.IsMatch(str, @"201[5,7](?:\dm\s\d\d?s)\b"))
+            {
+                str = Regex.Replace(str, PATTERN, REPLACE_PATTERN);
+            }
+            else if (Regex.IsMatch(str, @"\b([\s]+(?:\dm\s\d\d?s))\b"))
+            {
+                str = Regex.Replace(str, PATTERN, REPLACE_PATTERN);
             }
 
             return str;
